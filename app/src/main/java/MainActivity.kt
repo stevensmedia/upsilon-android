8a4cad18034c985b0abf8249e081a72807971f63
Neil Stevens <neil@stevensmedia.us>
2023-03-24 22:00:36 -0400

    Initial import

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,16 @@
+package us.hakubi.upsilon
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/simulator.light.html")
+    }
+}
